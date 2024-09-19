e7352f623d78f8324465b09c2475941d5fface56
Logan Cardoso <logan.cardoso@dgtallab.com.br>
2024-09-19 13:12:47 -0300

    fix: aterando o path das doações.

--- lib/app/modules/doacoes/doacoes_module.dart
+++ lib/app/modules/doacoes/doacoes_module.dart
@@ -3,7 +3,7 @@ import 'package:boi_marronzinho/app/modules/doacoes/doacoes_view.dart';
 import 'package:get/get.dart';
 
 abstract class DoacoesModule {
-  static const path = '/home';
+  static const path = '/doacoes';
 
   static GetPage page = GetPage(
     name: path,
