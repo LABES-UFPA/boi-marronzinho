f77e84d493b317f104c2a2610fc9313da357830a
Logan Cardoso <logan.cardoso@dgtallab.com.br>
2024-09-19 12:47:43 -0300

    fix: adicionando path para a tela de doações no botao da home

--- lib/app/modules/home_page/home_page_controller.dart
+++ lib/app/modules/home_page/home_page_controller.dart
@@ -1,4 +1,5 @@
 import 'package:boi_marronzinho/app/data/controllers/base_controller.dart';
+import 'package:boi_marronzinho/app/modules/doacoes/doacoes_module.dart';
 import 'package:boi_marronzinho/app/modules/home_page/home_page_module.dart';
 import 'package:get/get.dart';
 
@@ -25,7 +26,7 @@ class HomePageController extends BaseController {
   }
 
   void onDoeAgoraPressed() {
-    Get.toNamed(HomeModule.path);
+    Get.toNamed(DoacoesModule.path);
   }
 
 
