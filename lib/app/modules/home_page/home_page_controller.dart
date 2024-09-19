6e8c260a5fb4528e6a652e13f164bb0a33bc95da
Logan Cardoso <logan.cardoso@dgtallab.com.br>
2024-09-19 12:21:43 -0300

    wip: criando botões da home page nos controllers

--- lib/app/modules/home_page/home_page_controller.dart
+++ lib/app/modules/home_page/home_page_controller.dart
@@ -1,4 +1,6 @@
 import 'package:boi_marronzinho/app/data/controllers/base_controller.dart';
+import 'package:boi_marronzinho/app/modules/home_page/home_page_module.dart';
+import 'package:get/get.dart';
 
 class HomePageController extends BaseController {
 
@@ -6,5 +8,29 @@ class HomePageController extends BaseController {
   void onInit() {
     super.onInit();
 
+    getBoiCoins();
   }
+
+
+  void onEventosPressed() {
+    Get.toNamed(HomeModule.path);
+  }
+
+  void onMapaPressed() {
+    Get.toNamed(HomeModule.path);
+  }
+
+  void onSobreNosPressed() {
+    Get.toNamed(HomeModule.path);
+  }
+
+  void onDoeAgoraPressed() {
+    Get.toNamed(HomeModule.path);
+  }
+
+
+  Future<double> getBoiCoins() async {
+    return 0.1;
+  }
+
 }
