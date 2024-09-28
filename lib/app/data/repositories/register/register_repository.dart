48b7afd457f77a128b396678518982c5c859cf04
Logan Cardoso <logan.cardoso@dgtallab.com.br>
2024-09-28 19:27:15 -0300

    feat: implementando chamada a API do boi morranzinho

---
+++ lib/app/data/repositories/register/register_repository.dart
@@ -0,0 +1,41 @@
+import 'dart:io';
+import 'package:boi_marronzinho/app/data/repositories/register/register_repository.interface.dart';
+import 'package:boi_marronzinho/app/data/request_repository.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/services.dart';
+import '../../enumerators/endpoints.enum.dart';
+
+
+final class RegisterRepository extends RequestRepository implements IRegisterRepository {
+  static const String registerUser = '/usuarios/singup';
+
+  @override
+  Future register({
+    required String firstName,
+    required String lastName,
+    required String password,
+    required String email,
+  }) async {
+    final url = apiHelpers.buildUrl(url: registerUser, endpoint: Endpoints.BOI_MARRONZINHO);
+
+    final Map<String, dynamic> bodyRegister = {
+      'firstName': firstName,
+      'lastName': lastName,
+      'email': email,
+      'password': password,
+    };
+
+    try {
+      final response = await client.post(url, bodyRegister);
+      final invalidResponse = isValidResponse(response);
+      if (!invalidResponse.valid) {
+        return invalidResponse;
+      }
+
+      return (valid: true, reason: null, data: null);
+    } catch (error, trace) {
+      return errorResponse(error, trace: trace);
+    }
+  }
+
+}
