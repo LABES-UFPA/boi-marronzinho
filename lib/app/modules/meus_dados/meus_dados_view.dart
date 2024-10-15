9662737fd3134f57c986fa07164c1d21c9bcb04b
Thiago P. <33945251+Stopfield@users.noreply.github.com>
2024-10-15 15:02:41 -0300

    feat: completa a tela de Meus Dados;
    * Adiciona widget `UserIcon` que mostra o nome da pessoa;
    * Adiciona widget `UserInfoCard` que pode mostrar uma imagem, label e
      texto com informações gerais;
    * Ainda é necessário integração com a API

--- lib/app/modules/meus_dados/meus_dados_view.dart
+++ lib/app/modules/meus_dados/meus_dados_view.dart
@@ -6,9 +6,85 @@ import 'package:get/get.dart';
 class MeusDadosView extends GetView<MeusDadosController> {
   const MeusDadosView({super.key});
 
+  static const Color bgColor = Color(0xFFF69302);
+  static const String userImagePath = "assets/images/icons/user_alt.png";
+  static const String emailImagePath = "assets/images/icons/email.png";
+
   @override
   Widget build(BuildContext context) {
-    return Text("Testando");
+    return SafeArea(
+        child: Scaffold(
+            backgroundColor: bgColor,
+            body: Column(children: [
+              Stack(
+                children: [
+                  ClipPath(
+                    clipper: AppBarClipper(),
+                    child: Container(
+                      height: 100.h,
+                      decoration: const BoxDecoration(
+                        color: Color(0xFFFFFFFF),
+                      ),
+                      child: Padding(
+                        padding: const EdgeInsets.all(10).h,
+                        child: Row(
+                          children: [
+                            IconButton(
+                              icon: Image.asset(
+                                'assets/images/icons/mingcute_arrow-up-fill.png',
+                                height: 40.h,
+                                width: 40.w,
+                              ),
+                              onPressed: () {
+                                Get.back();
+                              },
+                            ),
+                            Expanded(
+                              child: Row(
+                                mainAxisAlignment: MainAxisAlignment.start,
+                                children: [
+                                  SizedBox(width: 10.w),
+                                  Icon(
+                                    Icons.account_box_rounded,
+                                    color: Colors.black,
+                                    size: 30.sp,
+                                  ),
+                                  SizedBox(width: 5.w),
+                                  Center(
+                                    child: Text(
+                                      'Meus Dados',
+                                      style: TextStyle(
+                                        fontSize: 36.sp,
+                                        fontWeight: FontWeight.bold,
+                                        color: Colors.black,
+                                      ),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              50.verticalSpace,
+              // Body -- Dados
+              const UserIcon("João da Silva"),
+              15.verticalSpace,
+              const UserInfoCard(
+                label: "Nome de Usuário",
+                iconImagePath: userImagePath,
+                text: "João da Silva",
+              ),
+              20.verticalSpace,
+              const UserInfoCard(
+                  label: "Email",
+                  iconImagePath: emailImagePath,
+                  text: "joaosilva123@exemplo.com")
+            ])));
   }
 }
 
@@ -28,4 +104,106 @@ class AppBarClipper extends CustomClipper<Path> {
 
   @override
   bool shouldReclip(CustomClipper<Path> oldClipper) => false;
+}
+
+// Widget para ícone e nome do usuário
+// Recebe parâmetro "name"
+class UserIcon extends StatelessWidget {
+  final String _name;
+  static const String _userImagePath = "assets/images/icons/user_alt.png";
+
+  const UserIcon(this._name, {super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        Container(
+            decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                border: Border.all(color: Colors.black, width: 4.2.w)),
+            child: CircleAvatar(
+              radius: 0.2.sw,
+              backgroundColor: Colors.white,
+              child: Image.asset(
+                _userImagePath,
+                fit: BoxFit.contain,
+                width: 100.w,
+              ),
+            )),
+        Text(
+          _name,
+          style: TextStyle(fontSize: 25.29.sp, fontWeight: FontWeight.bold),
+          textAlign: TextAlign.center,
+        ),
+      ],
+    );
+  }
+}
+
+// Contém informações genéricas de Nome de Usuário e Email
+// Recebe label, text e o endereço da imagem do ícone
+class UserInfoCard extends StatelessWidget {
+  final String label;
+  final String text;
+  final String iconImagePath;
+
+  const UserInfoCard(
+      {required this.label,
+      required this.iconImagePath,
+      required this.text,
+      super.key});
+
+  static final Radius _cardRadius = const Radius.circular(20).r;
+  static final TextStyle _labelStyle = TextStyle(
+      color: const Color(0xFFF69302),
+      fontSize: 20.sp,
+      fontWeight: FontWeight.bold);
+  static final TextStyle _textStyle =
+      TextStyle(color: Colors.black, fontSize: 16.sp);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 0.87.sw,
+      height: 84.h,
+      padding: EdgeInsets.all(16.w),
+      decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.only(
+              topLeft: _cardRadius,
+              bottomLeft: _cardRadius,
+              bottomRight: _cardRadius)),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          Image.asset(
+            iconImagePath,
+            width: 50.w,
+            height: 50.h,
+          ),
+          15.horizontalSpace,
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                label,
+                style: _labelStyle,
+                overflow: TextOverflow.fade,
+              ),
+              Text(
+                text,
+                style: _textStyle,
+                overflow: TextOverflow.fade,
+              )
+            ],
+          )
+        ],
+      ),
+    );
+  }
 }
