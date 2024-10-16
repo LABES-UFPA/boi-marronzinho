0735107fa5cd402d484f5c6504fa0e3f946f99a6
Thiago P. <33945251+Stopfield@users.noreply.github.com>
2024-10-16 14:10:31 -0300

    fix: retira strings do merge que esqueci de tirar

--- lib/app/modules/perfil/perfil_controller.dart
+++ lib/app/modules/perfil/perfil_controller.dart
@@ -1,10 +1,7 @@
 import 'package:boi_marronzinho/app/data/controllers/base_controller.dart';
-<<<<<<< HEAD
 import 'package:boi_marronzinho/app/modules/carteira/carteira_module.dart';
-=======
 import 'package:boi_marronzinho/app/data/storage/memory_storage.dart';
 import 'package:boi_marronzinho/app/modules/administrador/admin_module.dart';
->>>>>>> main
 import 'package:boi_marronzinho/app/modules/home_page/home_page_module.dart';
 import 'package:boi_marronzinho/app/modules/loja/loja_module.dart';
 import 'package:boi_marronzinho/app/modules/perfil/perfil_module.dart';
