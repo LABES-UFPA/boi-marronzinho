6c902f3f5a9d65f13c315dae837bbd82d049bcf0
Thiago P. <33945251+Stopfield@users.noreply.github.com>
2024-10-18 06:53:18 -0300

    feat: implementa inicio da integração com API
    * Ainda tem dados mockados, mas os dados já vem do Controller. Falta
      integrar com a API usando a rota;
    * Adiciona animação de loading no início;

--- lib/app/modules/meus_dados/meus_dados_controller.dart
+++ lib/app/modules/meus_dados/meus_dados_controller.dart
@@ -1,9 +1,24 @@
 import 'package:boi_marronzinho/app/data/controllers/base_controller.dart';
+import 'package:boi_marronzinho/app/data/repositories/meus_dados/meus_dados_repository.dart';
+import 'package:boi_marronzinho/app/data/repositories/voucher/voucher_repository.dart';
+import 'package:boi_marronzinho/app/modules/meus_dados/meus_dados_model.dart';
 
 class MeusDadosController extends BaseController {
+  final voucherRepo = VoucherRepository();
+  late MeusDados dadosExemplo;
+
   @override
   void onInit() {
     // TODO: implement onInit
     super.onInit();
+    getUserdata();
   }
+
+  void getUserdata() {
+    setLoading(true);
+    dadosExemplo = MeusDados(username: 'José Carlos', email: 'josecarlos@exemplo.com');
+    setLoading(false);
+    update();
+  }
+  
 }
