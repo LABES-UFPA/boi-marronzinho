9f60c1e8f66f84d892bcbe767f9cce77058b3a29
Logan Cardoso <logan.cc@outlook.com>
2024-10-23 20:20:00 -0300

    Update environments.dart

--- lib/app/data/constants/environments.dart
+++ lib/app/data/constants/environments.dart
@@ -3,7 +3,8 @@ import 'package:boi_marronzinho/app/data/models/constants/environment_properties
 const EnvironmentProperties LOCAL_ENV = EnvironmentProperties(
   cacheLifespan: 60000,
   endpoint: EndpointProperties(
-    projeto: 'https://12b5-45-228-141-19.ngrok-free.app/api/v1',
+    projeto: 'https://f17c-45-228-141-19.ngrok-free.app/api/v1',
+    minio: 'https://38c4-45-228-141-19.ngrok-free.app',
     timeout: 1000,
   ),
 );
@@ -11,7 +12,8 @@ const EnvironmentProperties LOCAL_ENV = EnvironmentProperties(
 const EnvironmentProperties PRODUCTION_ENV = EnvironmentProperties(
   cacheLifespan: 300000,
   endpoint: EndpointProperties(
-    projeto: 'https://12b5-45-228-141-19.ngrok-free.app/api/v1',
+    projeto: 'https://5337-45-228-141-19.ngrok-free.app/api/v1',
+    minio: 'https://38c4-45-228-141-19.ngrok-free.app',
     timeout: 80,
   ),
 );
